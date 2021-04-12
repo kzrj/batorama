15e3e7c64c93bddc437a98a24b1a4da6a7b75a4d
kzrj <kzrster@gmail.com>
2021-04-12 10:45:44 +0800

    employee login

--- batorama/stock/admin.py
+++ batorama/stock/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from stock.models import (Employee, Lumber, Shift, LumberRecord, CashRecord)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Employee._meta.fields]
+
+
+@admin.register(Lumber)
+class LumberAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Lumber._meta.fields]
+
+
+@admin.register(Shift)
+class ShiftAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Shift._meta.fields]
+
+
+@admin.register(LumberRecord)
+class LumberRecordAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in LumberRecord._meta.fields]
