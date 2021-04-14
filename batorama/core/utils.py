c5f4f049bc466cb0d9b4150a5cda234947762311
kzrj <kzrster@gmail.com>
2021-04-14 12:59:30 +0800

    new account ver

--- batorama/core/utils.py
+++ batorama/core/utils.py
@@ -65,5 +65,5 @@ class EmployeeSerializer(serializers.ModelSerializer):
 def jwt_response_payload_handler(token, user=None, request=None):
     return {
         'token': token,
-        'user': EmployeeSerializer(user.employee).data
+        'user': EmployeeSerializer(user.account).data
     }
