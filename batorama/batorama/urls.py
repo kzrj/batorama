18193e72d8e905327f0d2089a02e52f4de313dcb
kzrj <kzrster@gmail.com>
2021-04-16 12:57:45 +0800

    new apis

--- batorama/batorama/urls.py
+++ batorama/batorama/urls.py
@@ -6,16 +6,14 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from rest_framework import routers
-
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 from apis.manager_api import RamshikiPaymentViewSet, ShiftListView
 from apis.ramshik_api import ShiftViewSet, InitTestDataView
 
-router = routers.DefaultRouter()
-router.register(r'shifts', ShiftViewSet, basename='shifts')
-router.register(r'manager/ramshik_payments', RamshikiPaymentViewSet, basename='ramshik_payments')
+# router = routers.DefaultRouter()
+# router.register(r'shifts', ShiftViewSet, basename='shifts')
+# router.register(r'manager/ramshik_payments', RamshikiPaymentViewSet, basename='ramshik_payments')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +25,9 @@ urlpatterns = [
 
     path('api/manager/ramshik_payments/init_data/', RamshikiPaymentViewSet.as_view({'get': 'init_data'})),
     path('api/manager/ramshik_payments/ramshik_payout/', RamshikiPaymentViewSet.as_view({'post': 'ramshik_payout'})),
+    path('api/manager/shift_list/', ShiftListView.as_view({'post': 'list'})),
+
+
 
 
 
