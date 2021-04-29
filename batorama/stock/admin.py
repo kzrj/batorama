b93c5371e0412a844ef52cde99b2e0a6861dd9d1
kzrj <kzrster@gmail.com>
2021-04-29 08:24:08 +0800

    front

--- batorama/stock/admin.py
+++ batorama/stock/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from stock.models import (Lumber, Shift, LumberRecord)
+from stock.models import (Lumber, Shift, LumberRecord, Sale)
 
 
 @admin.register(Lumber)
@@ -11,6 +11,11 @@ class LumberAdmin(admin.ModelAdmin):
 @admin.register(Shift)
 class ShiftAdmin(admin.ModelAdmin):
     list_display = [f.name for f in Shift._meta.fields]
+
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Sale._meta.fields]
 
 
 @admin.register(LumberRecord)
