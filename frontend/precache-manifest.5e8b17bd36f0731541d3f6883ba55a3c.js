cf6e90d4f0472e280bdcc665fbbfb874d387af31
kzrj <kzrster@gmail.com>
2021-05-17 17:44:37 +0800

    lumber round_volume

--- frontend/precache-manifest.5e8b17bd36f0731541d3f6883ba55a3c.js
+++ frontend/precache-manifest.5e8b17bd36f0731541d3f6883ba55a3c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "26351c75dbe768689b73d03000fa2244",
+    "revision": "eab339ffa6f26c8e66971837fc6f94a2",
     "url": "/index.html"
   },
   {
-    "revision": "cebcdc8319c7573f4e3e",
-    "url": "/static/js/2.016a047e.chunk.js"
+    "revision": "3c8bac0c6652ed358200",
+    "url": "/static/js/2.c5877a9d.chunk.js"
   },
   {
     "revision": "385801f39fe2615b5790da562b481c6c",
-    "url": "/static/js/2.016a047e.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.c5877a9d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "854a310c2e2b46721610",
-    "url": "/static/js/main.06cdaccd.chunk.js"
+    "revision": "f649c84c1e1eaf6992ca",
+    "url": "/static/js/main.dcb521d7.chunk.js"
   },
   {
     "revision": "8bba03c2c12330ef7b9f",
--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f695a56daee41892663c9ef6b22a4667.js"
+  "/precache-manifest.5e8b17bd36f0731541d3f6883ba55a3c.js"
 );
 
 self.addEventListener('message', (event) => {
