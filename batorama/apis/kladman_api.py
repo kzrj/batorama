c424c0ec65c7188f14666f9422446bef00c9c44d
kzrj <kzrster@gmail.com>
2021-05-21 13:41:46 +0800

    expenses

--- batorama/apis/kladman_api.py
+++ batorama/apis/kladman_api.py
@@ -1,6 +1,6 @@
 # # -*- coding: utf-8 -*-
 from django.contrib.auth.models import User
-from django.db.models import Q
+from django.db.models import Q, Sum
 from django.utils import timezone
 
 from rest_framework.views import APIView
