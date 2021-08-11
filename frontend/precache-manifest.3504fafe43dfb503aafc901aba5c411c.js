122b7b270990887b7e4d992e34697cbdba8bcd4f
kzrj <kzrster@gmail.com>
2021-08-11 16:56:44 +0800

    saler lumbers

--- frontend/precache-manifest.3504fafe43dfb503aafc901aba5c411c.js
+++ frontend/precache-manifest.3504fafe43dfb503aafc901aba5c411c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5fc463520896e95cf5db0bd70cac29b4",
+    "revision": "cebd065ca6f9b93fac7f288fbdd28373",
     "url": "/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.5da22b83.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "eca929f60eb94b34b478",
-    "url": "/static/js/main.92112c92.chunk.js"
+    "revision": "875f366e219398a893f5",
+    "url": "/static/js/main.5a0369fb.chunk.js"
   },
   {
     "revision": "8bba03c2c12330ef7b9f",
